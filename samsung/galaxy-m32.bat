38c7dd81fbe6b7eac521e5bd2140be7a7153cf3a
Dat <38286831+datlp@users.noreply.github.com>
2022-11-13 11:58:04 +0700

    Update galaxy-m32.bat

--- samsung/galaxy-m32.bat
+++ samsung/galaxy-m32.bat
@@ -1,85 +1,81 @@
-adb shell pm uninstall --user 0 com.android.bips || break on || Default Print Service
-adb shell pm uninstall --user 0 com.android.hotwordenrollment.okgoogle
-adb shell pm uninstall --user 0 com.android.hotwordenrollment.xgoogle || break on || Google Assistant
-adb shell pm uninstall --user 0 com.android.printspooler || break on || Print Spooler
-adb shell pm uninstall --user 0 com.android.stk || break on || Sim 1 App 
-adb shell pm uninstall --user 0 com.android.stk2 || break on || Sim 2 App 
-adb shell pm uninstall --user 0 com.aura.oobe.samsung.gl || break on || appcloud
-adb shell pm uninstall --user 0 com.dsi.ant.plugins.antplus || break on || ANT+ Sport
-adb shell pm uninstall --user 0 com.dsi.ant.sample.acquirechannels || break on || ANT+ Sport
-adb shell pm uninstall --user 0 com.dsi.ant.server || break on || ANT+ Sport
-adb shell pm uninstall --user 0 com.dsi.ant.service.socket || break on || ANT+ Sport
-adb shell pm uninstall --user 0 com.facebook.appmanager || break on || Facebook
-adb shell pm uninstall --user 0 com.facebook.services || break on || Facebook
-adb shell pm uninstall --user 0 com.facebook.system || break on || Facebook
-adb shell pm uninstall --user 0 com.google.android.apps.restore || break on || android setup
-adb shell pm uninstall --user 0 com.google.android.apps.tachyon || break on || Meet
+adb shell pm uninstall --user 0 com.samsungapp.newtrim
+adb shell pm uninstall --user 0 com.android.bips
+adb shell pm uninstall --user 0 com.android.printspooler
+adb shell pm uninstall --user 0 com.android.stk
+adb shell pm uninstall --user 0 com.android.stk2
+adb shell pm uninstall --user 0 com.aura.oobe.samsung.gl
+adb shell pm uninstall --user 0 com.dsi.ant.plugins.antplus
+adb shell pm uninstall --user 0 com.dsi.ant.sample.acquirechannels
+adb shell pm uninstall --user 0 com.dsi.ant.server
+adb shell pm uninstall --user 0 com.dsi.ant.service.socket
+adb shell pm uninstall --user 0 com.facebook.appmanager
+adb shell pm uninstall --user 0 com.facebook.services
+adb shell pm uninstall --user 0 com.facebook.system
+adb shell pm uninstall --user 0 com.google.android.apps.restore
+adb shell pm uninstall --user 0 com.google.android.apps.tachyon
 adb shell pm uninstall --user 0 com.google.android.apps.turbo
-adb shell pm uninstall --user 0 com.google.android.as || break on || Android System Intelligence
-adb shell pm uninstall --user 0 com.google.android.cellbroadcastreceiver || break on || Wireless emergency alerts
-adb shell pm uninstall --user 0 com.google.android.gm || break on || gmail
-adb shell pm uninstall --user 0 com.google.android.printservice.recommendation || break on || Print Service Recommendation Service
-adb shell pm uninstall --user 0 com.google.android.projection.gearhead || break on || Android auto
+adb shell pm uninstall --user 0 com.google.android.as
+adb shell pm uninstall --user 0 com.google.android.cellbroadcastreceiver
+adb shell pm uninstall --user 0 com.google.android.gm
+adb shell pm uninstall --user 0 com.google.android.printservice.recommendation
+adb shell pm uninstall --user 0 com.google.android.projection.gearhead
 adb shell pm uninstall --user 0 com.google.android.setupwizard
-adb shell pm uninstall --user 0 com.google.android.youtube || break on || YouTube Stock
-adb shell pm uninstall --user 0 com.google.ar.core || break on || Google Play Services for AR
-adb shell pm uninstall --user 0 com.google.ar.core || break on || Google Play Services for AR 
-adb shell pm uninstall --user 0 com.google.audio.hearing.visualization.accessibility.scribe || break on || Live Transcribe & Sound Notifications
-adb shell pm uninstall --user 0 com.microsoft.skydrive || break on || OneDrive
-adb shell pm uninstall --user 0 com.netflix.mediaclient || break on || netflix
-adb shell pm uninstall --user 0 com.netflix.partner.activation || break on || netflix
-adb shell pm uninstall --user 0 com.samsung.android.app.appsedge || break on || Edge
-adb shell pm uninstall --user 0 com.samsung.android.app.clipboardedge || break on || Edge
-adb shell pm uninstall --user 0 com.samsung.android.app.cocktailbarservice || break on || Edge panels
+adb shell pm uninstall --user 0 com.google.android.youtube
+adb shell pm uninstall --user 0 com.google.ar.core
+adb shell pm uninstall --user 0 com.google.audio.hearing.visualization.accessibility.scribe
+adb shell pm uninstall --user 0 com.microsoft.skydrive
+adb shell pm uninstall --user 0 com.netflix.mediaclient
+adb shell pm uninstall --user 0 com.netflix.partner.activation
+adb shell pm uninstall --user 0 com.samsung.android.app.appsedge
+adb shell pm uninstall --user 0 com.samsung.android.app.clipboardedge
+adb shell pm uninstall --user 0 com.samsung.android.app.cocktailbarservice
 adb shell pm uninstall --user 0 com.samsung.android.app.omcagent
-adb shell pm uninstall --user 0 com.samsung.android.app.reminder || break on || Reminder
+adb shell pm uninstall --user 0 com.samsung.android.app.reminder
 adb shell pm uninstall --user 0 com.samsung.android.app.routines
-adb shell pm uninstall --user 0 com.samsung.android.app.settings.bixby || break on || Bixby
-adb shell pm uninstall --user 0 com.samsung.android.app.spage || break on || samsung free
-adb shell pm uninstall --user 0 com.samsung.android.app.taskedge || break on || Edge
-adb shell pm uninstall --user 0 com.samsung.android.app.tips || break on || Tips
-adb shell pm uninstall --user 0 com.samsung.android.app.updatecenter || break on || App update
+adb shell pm uninstall --user 0 com.samsung.android.app.settings.bixby
+adb shell pm uninstall --user 0 com.samsung.android.app.spage
+adb shell pm uninstall --user 0 com.samsung.android.app.taskedge
+adb shell pm uninstall --user 0 com.samsung.android.app.tips
+adb shell pm uninstall --user 0 com.samsung.android.app.updatecenter
 adb shell pm uninstall --user 0 com.samsung.android.app.watchmanagerstub
-adb shell pm uninstall --user 0 com.samsung.android.ardrawing || break on || AR Doodle
-adb shell pm uninstall --user 0 com.samsung.android.aremoji || break on || AR Emoji
+adb shell pm uninstall --user 0 com.samsung.android.ardrawing
+adb shell pm uninstall --user 0 com.samsung.android.aremoji
 adb shell pm uninstall --user 0 com.samsung.android.aremojieditor
 adb shell pm uninstall --user 0 com.samsung.android.arzone
-adb shell pm uninstall --user 0 com.samsung.android.authfw || break on || Authentication Framework
-adb shell pm uninstall --user 0 com.samsung.android.bbc.bbcagent || break on || BBC
-adb shell pm uninstall --user 0 com.samsung.android.bixby.agent || break on || Bixby
-adb shell pm uninstall --user 0 com.samsung.android.bixby.agent.dummy || break on || Bixby
-adb shell pm uninstall --user 0 com.samsung.android.bixby.service || break on || Bixby
-adb shell pm uninstall --user 0 com.samsung.android.bixby.wakeup || break on || Bixby
-adb shell pm uninstall --user 0 com.samsung.android.bixbyvision.framework || break on || Bixby
-adb shell pm uninstall --user 0 com.samsung.android.calendar || break on || Calendar
-adb shell pm uninstall --user 0 com.samsung.android.dynamiclock || break on || Dynamic Lockscreen
-adb shell pm uninstall --user 0 com.samsung.android.fmm || break on || Tìm di động của bạn 
-adb shell pm uninstall --user 0 com.samsung.android.fores || break on || Digital Wellbeing
-adb shell pm uninstall --user 0 com.samsung.android.game.gamehome || break on || Game
-adb shell pm uninstall --user 0 com.samsung.android.game.gametools || break on || Game
-adb shell pm uninstall --user 0 com.samsung.android.game.gos || break on || Game Optimizing Service
+adb shell pm uninstall --user 0 com.samsung.android.authfw
+adb shell pm uninstall --user 0 com.samsung.android.bbc.bbcagent
+adb shell pm uninstall --user 0 com.samsung.android.bixby.agent
+adb shell pm uninstall --user 0 com.samsung.android.bixby.agent.dummy
+adb shell pm uninstall --user 0 com.samsung.android.bixby.service
+adb shell pm uninstall --user 0 com.samsung.android.bixby.wakeup
+adb shell pm uninstall --user 0 com.samsung.android.bixbyvision.framework
+adb shell pm uninstall --user 0 com.samsung.android.calendar
+adb shell pm uninstall --user 0 com.samsung.android.dynamiclock
+adb shell pm uninstall --user 0 com.samsung.android.fmm
+adb shell pm uninstall --user 0 com.samsung.android.fores
+adb shell pm uninstall --user 0 com.samsung.android.game.gamehome
+adb shell pm uninstall --user 0 com.samsung.android.game.gametools
+adb shell pm uninstall --user 0 com.samsung.android.game.gos
 adb shell pm uninstall --user 0 com.samsung.android.homemode
-adb shell pm uninstall --user 0 com.samsung.android.icecone || break on || Keyboard Content Center
-adb shell pm uninstall --user 0 com.samsung.android.livestickers || break on || DECO PIC
-adb shell pm uninstall --user 0 com.samsung.android.lool || break on || Device care
+adb shell pm uninstall --user 0 com.samsung.android.icecone
+adb shell pm uninstall --user 0 com.samsung.android.livestickers
+adb shell pm uninstall --user 0 com.samsung.android.lool
 adb shell pm uninstall --user 0 com.samsung.android.mapsagent
 adb shell pm uninstall --user 0 com.samsung.android.mdx.quickboard
-adb shell pm uninstall --user 0 com.samsung.android.mdx.quickboard || break on || Media and devices
 adb shell pm uninstall --user 0 com.samsung.android.samsungpass
 adb shell pm uninstall --user 0 com.samsung.android.samsungpassautofill
-adb shell pm uninstall --user 0 com.samsung.android.service.livedrawing || break on || Live messages
-adb shell pm uninstall --user 0 com.samsung.android.stickercenter || break on || Sticker Center
-adb shell pm uninstall --user 0 com.samsung.android.video || break on || Video Player
+adb shell pm uninstall --user 0 com.samsung.android.service.livedrawing
+adb shell pm uninstall --user 0 com.samsung.android.stickercenter
+adb shell pm uninstall --user 0 com.samsung.android.video
 adb shell pm uninstall --user 0 com.samsung.android.visionintelligence
-adb shell pm uninstall --user 0 com.samsung.app.newtrim || break on || Video trimmer
-adb shell pm uninstall --user 0 com.samsung.systemui.bixby2 || break on || Bixby
-adb shell pm uninstall --user 0 com.sec.android.app.billing || break on || Samsung Checkout
-adb shell pm uninstall --user 0 com.sec.android.app.clockpackage || break on || Samsung Clock
-adb shell pm uninstall --user 0 com.sec.android.app.fm || break on || FM Radio
-adb shell pm uninstall --user 0 com.sec.android.app.wlantest || break on || ANT+ Sport
-adb shell pm uninstall --user 0 com.sec.android.daemonapp || break on || Weather
-adb shell pm uninstall --user 0 com.sec.android.mimage.avatarstickers || break on || AR Emoji Stickers
-adb shell pm uninstall --user 0 com.sec.android.soagent || break on || disable update
-adb shell pm uninstall --user 0 com.sec.android.widgetapp.webmanual || break on || User manual
-adb shell pm uninstall --user 0 com.sec.spp.push || break on || Samsung Push Service
-adb shell pm uninstall --user 0 com.wssyncmldm || break on || disable update
+adb shell pm uninstall --user 0 com.samsung.systemui.bixby2
+adb shell pm uninstall --user 0 com.sec.android.app.billing
+adb shell pm uninstall --user 0 com.sec.android.app.clockpackage
+adb shell pm uninstall --user 0 com.sec.android.app.fm
+adb shell pm uninstall --user 0 com.sec.android.app.wlantest
+adb shell pm uninstall --user 0 com.sec.android.daemonapp
+adb shell pm uninstall --user 0 com.sec.android.mimage.avatarstickers
+adb shell pm uninstall --user 0 com.sec.android.soagent
+adb shell pm uninstall --user 0 com.sec.android.widgetapp.webmanual
+adb shell pm uninstall --user 0 com.sec.spp.push
+adb shell pm uninstall --user 0 com.wssyncmldm
